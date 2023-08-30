bb723d9e0ca64150a814b750254ba70d9cde6924
yumXD <qkpw99@gmail.com>
2023-08-30 11:44:00 +0900

    feat: 타임리프 레이아웃 적용

---
+++ src/main/java/com/salary/controller/MainViewController.java
@@ -0,0 +1,12 @@
+package com.salary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainViewController {
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+}

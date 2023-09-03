fd0fd566809dbf08616998ac9e960a0659dc9933
yumXD <qkpw99@gmail.com>
2023-09-03 15:54:00 +0900

    refactor: 로그아웃 POST 처리

--- src/main/java/com/salary/config/SecurityConfig.java
+++ src/main/java/com/salary/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +23,7 @@ public class SecurityConfig {
                 )
                 .logout(logout ->
                         logout
-                                .logoutUrl("/users/logout")
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/users/logout", "POST"))
                                 .logoutSuccessUrl("/")
                                 .invalidateHttpSession(true)
                                 .deleteCookies("JSESSIONID")

726442aff82447cbce469b473405ba3818f2e38b
yumXD <qkpw99@gmail.com>
2023-09-28 11:21:00 +0900

    feat: 직원 상세 정보 페이지 처리

--- src/main/java/com/salary/controller/EmployeeViewController.java
+++ src/main/java/com/salary/controller/EmployeeViewController.java
@@ -2,7 +2,9 @@ package com.salary.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,5 +17,11 @@ public class EmployeeViewController {
     @GetMapping("/employee/new")
     public String register() {
         return "employee/register";
+    }
+
+    @GetMapping("/employee/{id}")
+    public String employeeDtl(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("id", id);
+        return "employee/employeeDtl";
     }
 }

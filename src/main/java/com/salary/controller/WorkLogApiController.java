a18e3daa4700fcf3abb16d84a24dc946509dda0c
yumXD <qkpw99@gmail.com>
2023-10-18 13:06:41 +0900

    refactor: 근무 기록 관련 로깅 & 페이지 수정

--- src/main/java/com/salary/controller/WorkLogApiController.java
+++ src/main/java/com/salary/controller/WorkLogApiController.java
@@ -6,6 +6,7 @@ import com.salary.dto.WorkLogResponse;
 import com.salary.service.WorkLogService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
@@ -16,14 +17,15 @@ import java.net.URI;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/employees/{employeeId}/work-detail")
+@RequestMapping("/api/employees/{employeeId}/work-detail/work-log")
+@Slf4j
 public class WorkLogApiController {
     private final WorkLogService workLogService;
 
-    // 근무 기록 추가
-    @PostMapping("/work-log")
+    @PostMapping
     public ResponseEntity<WorkLog> createWorkLog(@PathVariable Long employeeId, @Valid @RequestBody WorkLogRequest workLogRequest) {
         WorkLog savedWorkLog = workLogService.createWorkLog(employeeId, workLogRequest);
+        log.info("{} 님 근무 기록 추가 완료", savedWorkLog.getWorkDetail().getEmployee().getName());
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(savedWorkLog.getId())
@@ -31,35 +33,34 @@ public class WorkLogApiController {
         return ResponseEntity.created(location).body(savedWorkLog);
     }
 
-    // 모든 근무 기록 조회
-    @GetMapping("/work-log")
+    @GetMapping
     public ResponseEntity<Page<WorkLogResponse>> findAllWorkLogs(@PathVariable Long employeeId,
                                                                  @RequestParam(value = "page", defaultValue = "0") int page,
                                                                  @RequestParam(value = "size", defaultValue = "3") int size) {
         Page<WorkLog> workLogPage = workLogService.findAll(employeeId, PageRequest.of(page, size));
-
+        log.info("{}의 모든 근무 기록 조회 완료", employeeId);
         Page<WorkLogResponse> workLogs = workLogPage.map(WorkLogResponse::new);
         return ResponseEntity.ok().body(workLogs);
     }
 
-    // 특정 근무 기록 조회
-    @GetMapping("/work-log/{workLogId}")
+    @GetMapping("/{workLogId}")
     public ResponseEntity<WorkLogResponse> findWorkLog(@PathVariable Long employeeId, @PathVariable Long workLogId) {
         WorkLog workLog = workLogService.findById(employeeId, workLogId);
+        log.info("{} 님 근무 기록 상세 조회 완료", workLog.getWorkDetail().getEmployee().getName());
         return ResponseEntity.ok().body(new WorkLogResponse(workLog));
     }
 
-    //특정 근무 기록 수정
-    @PutMapping("/work-log/{workLogId}")
+    @PutMapping("/{workLogId}")
     public ResponseEntity<WorkLog> updateWorkLog(@PathVariable Long employeeId, @PathVariable Long workLogId, @Valid @RequestBody WorkLogRequest workLogRequest) {
         WorkLog workLog = workLogService.update(employeeId, workLogId, workLogRequest);
+        log.info("{} 님 근무 기록 수정 완료", workLog.getWorkDetail().getEmployee().getName());
         return ResponseEntity.ok().body(workLog);
     }
 
-    //특정 근무 기록 삭제
-    @DeleteMapping("/work-log/{workLogId}")
+    @DeleteMapping("/{workLogId}")
     public ResponseEntity<Void> deleteWorkLog(@PathVariable Long employeeId, @PathVariable Long workLogId) {
         workLogService.delete(employeeId, workLogId);
+        log.info("근무 기록 삭제 완료");
         return ResponseEntity.ok()
                 .build();
     }
